38474a73b8c9f036fbda6672a66daac87d994974
Fabian Roscher <roscher2003@outlook.de>
2022-12-05 18:21:16 +0100

    Added second task

--- 05.v
+++ 05.v
@@ -1,8 +1,8 @@
 import os
-
-fn first(input []string)string{
-	mut breaked := 0
+// returns an array containing the inputed piles and and int wehere the new line was found and the move begins
+fn get_piles(input []string)([][]string, int){
 	mut piles := [][]string{len:9, init: []string{}}
+	mut breaked := 0
 	for i in 0 .. input.len {
 		// when \n is found the move statements are comming next and the i can break out because if have found all piles
 		if input[i] == ""{
@@ -21,6 +21,11 @@ fn first(input []string)string{
 			a++
 		}
 	}
+	return piles, breaked
+}
+
+fn first(input []string)string{
+	mut piles, breaked := get_piles(input)
 	for i in breaked + 1 .. input.len{
 		mut amount := input[i][5..7].int()
 
@@ -30,7 +35,6 @@ fn first(input []string)string{
 		} else {
 			from = piles[input[i][12..14].int() - 1]
 		}
-		println(piles)
 		mut to := piles[input[i][input[i].len - 1].ascii_str().int() - 1]
 		for _ in 0 .. amount{
 			if from.len >= 1{
@@ -46,7 +50,43 @@ fn first(input []string)string{
 		}
 		piles[input[i][input[i].len - 1].ascii_str().int() - 1].clear()
 		piles[input[i][input[i].len - 1].ascii_str().int() - 1].insert(0, to)
-		println(piles)
+	}
+	mut s := ""
+	for i in piles{
+		if i != []{
+			s += i.last()
+		}
+
+	}
+	return s
+}
+
+fn second(input []string)string{
+	mut piles, breaked := get_piles(input)
+	for i in breaked + 1 .. input.len{
+		mut amount := input[i][5..7].int()
+		mut from := []string{}
+		if amount > 9 {
+			from = piles[input[i][13..15].int() - 1]
+		} else {
+			from = piles[input[i][12..14].int() - 1]
+		}
+		mut to := piles[input[i][input[i].len - 1].ascii_str().int() - 1]
+		for j in from.len - amount .. from.len{
+			if from.len >= 1{
+				to.insert(to.len, from[j])
+			}
+		}
+		from.delete_many(from.len - amount, amount)
+		if amount > 9{
+			piles[input[i][13..15].int() - 1].clear()
+			piles[input[i][13..15].int() - 1].insert(0, from)
+		}else{
+			piles[input[i][12..14].int() - 1].clear()
+			piles[input[i][12..14].int() - 1].insert(0, from)
+		}
+		piles[input[i][input[i].len - 1].ascii_str().int() - 1].clear()
+		piles[input[i][input[i].len - 1].ascii_str().int() - 1].insert(0, to)
 	}
 	mut s := ""
 	for i in piles{
@@ -65,7 +105,6 @@ fn main(){
 	if os.is_file(path) == true {
 		content << os.read_lines(path) or { panic('not read: ${path}') }
 	}
-
 	println(first(content))
-	// println(second(content))
+	println(second(content))
 }
