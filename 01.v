69e8ed8fd33df7b93d7cb9ab3d1980e307a1d553
Fabian Roscher <roscher2003@outlook.de>
2022-12-05 19:59:46 +0100

    Added Comments

--- 01.v
+++ 01.v
@@ -8,15 +8,17 @@ if os.is_file(path) == true {
 	content << os.read_lines(path) or { panic('not read: ${path}') }
 }
 mut sum := 0
-mut highest_val := []int{len:3, init:0}
+mut highest_val := []int{len: 3, init: 0}
 for i in content {
+	// true when we have a new line so I need to add the sum to my highest val
 	if i == '' {
+		// setting the values correctly for the cases if one is higher than the other
 		if highest_val[2] < sum {
 			highest_val = [highest_val[1], highest_val[2], sum]
-		}else if highest_val[1] < sum{
+		} else if highest_val[1] < sum {
 			highest_val[0] = highest_val[1]
 			highest_val[1] = sum
-		}else if highest_val[0] < sum{
+		} else if highest_val[0] < sum {
 			highest_val[0] = sum
 		}
 		sum = 0
