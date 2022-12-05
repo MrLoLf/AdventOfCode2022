656df94d20ac0eb60c4c01a4bdf2036066c4c1f7
Fabian Roscher <roscher2003@outlook.de>
2022-12-05 19:36:58 +0100

    Added second part

--- 01.v
+++ 01.v
@@ -8,15 +8,21 @@ if os.is_file(path) == true {
 	content << os.read_lines(path) or { panic('not read: ${path}') }
 }
 mut sum := 0
-mut highest_val := 0
+mut highest_val := []int{len:3, init:0}
 for i in content {
 	if i == '' {
-		if highest_val < sum {
-			highest_val = sum
+		if highest_val[2] < sum {
+			highest_val = [highest_val[1], highest_val[2], sum]
+		}else if highest_val[1] < sum{
+			highest_val[0] = highest_val[1]
+			highest_val[1] = sum
+		}else if highest_val[0] < sum{
+			highest_val[0] = sum
 		}
 		sum = 0
 		continue
 	}
 	sum += i.int()
 }
-println(highest_val)
+println(highest_val[2])
+println(highest_val[0] + highest_val[1] + highest_val[2])
